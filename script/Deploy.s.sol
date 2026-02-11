c98645eec51325700d3cfefbe29cdf5b5a45115c
Ozov7 <114500806+Ozov7@users.noreply.github.com>
2026-02-11 15:52:02 +0100

    Enhance DeployScript with SecurityOrchestrator
    Added deployment of SecurityOrchestrator and improved logging.

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -6,6 +6,7 @@ import "../src/traps/MEVSandwichDetector.sol";
 import "../src/traps/GovernanceAttackMonitor.sol";
 import "../src/traps/OracleManipulationDetector.sol";
 import "../src/responders/SecurityResponder.sol";
+import "../src/orchestrator/SecurityOrchestrator.sol";
 
 contract DeployScript is Script {
     function run() external {
@@ -13,18 +14,37 @@ contract DeployScript is Script {
         
         vm.startBroadcast(deployerPrivateKey);
         
+        // Deploy responder
         SecurityResponder responder = new SecurityResponder();
-        console.log("Responder deployed at: %s", address(responder));
+        console.log("Responder deployed at:", address(responder));
         
+        // Deploy individual traps
         MEVSandwichDetector mevTrap = new MEVSandwichDetector();
-        console.log("MEV Trap deployed at: %s", address(mevTrap));
+        console.log("MEV Trap deployed at:", address(mevTrap));
         
         GovernanceAttackMonitor govTrap = new GovernanceAttackMonitor();
-        console.log("Governance Trap deployed at: %s", address(govTrap));
+        console.log("Governance Trap deployed at:", address(govTrap));
         
         OracleManipulationDetector oracleTrap = new OracleManipulationDetector();
-        console.log("Oracle Trap deployed at: %s", address(oracleTrap));
+        console.log("Oracle Trap deployed at:", address(oracleTrap));
+        
+        // Deploy unified orchestrator
+        SecurityOrchestrator orchestrator = new SecurityOrchestrator(
+            address(mevTrap),
+            address(govTrap),
+            address(oracleTrap)
+        );
+        console.log("Security Orchestrator deployed at:", address(orchestrator));
         
         vm.stopBroadcast();
+        
+        // Configuration output
+        console.log("\nDrosera Configuration:");
+        console.log("=======================");
+        console.log("\n[trap.security_orchestrator]");
+        console.log("address = \"%s\"", address(orchestrator));
+        console.log("response_contract = \"%s\"", address(responder));
+        console.log("response_function = \"handleCrossVectorAlert\"");
+        console.log("block_sample_size = 5");
     }
 }
